040916b246f7b15bcbcf10f8ac48b93e42717b15
AccountZero <hubert.korona@gmail.com>
2026-02-28 22:32:12 +0100

    fix: refactor code

--- src/main/java/me/accountzero/morecraft/events/CustomMobs.java
+++ src/main/java/me/accountzero/morecraft/events/CustomMobs.java
@@ -8,56 +8,63 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.List;
+import java.util.Random;
 
 public class CustomMobs implements Listener {
+    Random random = new Random();
     @EventHandler
     public void onEntitySpawn(EntitySpawnEvent event) {
         if (event.getEntityType() == EntityType.ZOMBIE) {
             Zombie zombie = (Zombie) event.getEntity();
             zombie.getEquipment().setHelmet(new ItemStack(Material.LEATHER_HELMET));
-            zombie.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.3);
-            zombie.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH).setBaseValue(30);
+            zombie.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.3);
+            zombie.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(30);
             zombie.setHealth(30.0);
         }
         if (event.getEntityType() == EntityType.SKELETON) {
             Skeleton skeleton = (Skeleton) event.getEntity();
             skeleton.getEquipment().setHelmet(new ItemStack(Material.LEATHER_HELMET));
-            skeleton.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.35);
-            skeleton.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH).setBaseValue(30);
+            skeleton.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.35);
+            skeleton.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(30);
             skeleton.setHealth(30.0);
         }
         if (event.getEntityType() == EntityType.SPIDER) {
             Spider spider = (Spider) event.getEntity();
-            spider.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.69);
-            spider.getAttribute(org.bukkit.attribute.Attribute.GENERIC_JUMP_STRENGTH).setBaseValue(1.8);
+            spider.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.69);
+            spider.getAttribute(Attribute.GENERIC_JUMP_STRENGTH).setBaseValue(0.8);
             spider.getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(0.5);
-            spider.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH).setBaseValue(24);
+            spider.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(24);
             spider.setHealth(24.0);
         }
-
         if (event.getEntityType() == EntityType.RABBIT) {
             Rabbit rabbit = (Rabbit) event.getEntity();
-            rabbit.getAttribute(org.bukkit.attribute.Attribute.GENERIC_JUMP_STRENGTH).setBaseValue(1.2);
+            rabbit.getAttribute(Attribute.GENERIC_JUMP_STRENGTH).setBaseValue(1.2);
             rabbit.getAttribute(Attribute.GENERIC_SAFE_FALL_DISTANCE).setBaseValue(8);
             rabbit.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(6);
             rabbit.setHealth(6.0);
         }
         if (event.getEntityType() == EntityType.IRON_GOLEM) {
-            IronGolem iron_golem = (IronGolem) event.getEntity();
-            iron_golem.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.50);
-            iron_golem.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH).setBaseValue(150);
-            iron_golem.setHealth(150.0);
+            IronGolem ironGolem = (IronGolem) event.getEntity();
+            ironGolem.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.50);
+            ironGolem.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(150);
+            ironGolem.setHealth(150.0);
         }
         if (event.getEntityType() == EntityType.SNOW_GOLEM) {
             // They will not die when exposed to sun + maybe repurpose as sentry gun ?
-            Snowman snow_golem = (Snowman) event.getEntity();
-            snow_golem.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH).setBaseValue(40);
-            snow_golem.setHealth(40.0);
+            Snowman snowGolem = (Snowman) event.getEntity();
+            snowGolem.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40);
+            snowGolem.setHealth(40.0);
         }
         if (event.getEntityType() == EntityType.PHANTOM) {
             Phantom phantom = (Phantom) event.getEntity();
-            phantom.getAttribute(org.bukkit.attribute.Attribute.GENERIC_MAX_HEALTH).setBaseValue(40);
+            phantom.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40);
             phantom.setHealth(40.0);
         }
         if (event.getEntity() instanceof LivingEntity) {
